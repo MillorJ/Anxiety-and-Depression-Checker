087ef6dc032bb7fe1bb377af87e6beb096912222
John Rey Millor <johnreymillor69@gmail.com>
2024-09-12 21:41:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let currentSection = 1;
 
 function showSection(sectionNumber) {
     const totalSections = 3;
+
+// Hide current section
+document.getElementById(`section${currentSection}`).classList.remove('active');
+document.getElementById(`section${currentSection}`).classList.add('hidden');
